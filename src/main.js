9a72158e38a988a8af96db1015b4cb88ecdbc401
Vasco Félix <vascorf18@gmail.com>
2023-05-20 19:26:46 +0100

    main restored

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import * as THREE from "three";
-import camera from "./camera.js";
-import arm from "./braco.js";
+import cameraControl from "./camera.js";
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
+
+var camera;
 
 function animate() {
     // tldr, everytime the program has time to render a frame, it'll call this
     // function
     requestAnimationFrame(animate);
-    renderer.render(scene, camera.raw);
+    renderer.render(scene, camera);
 }
 
 function main() {
@@ -18,9 +19,8 @@ function main() {
     document.body.appendChild(renderer.domElement);
 
     scene.add(new THREE.AxesHelper(400));
-    scene.add(arm.leftArm);
-    scene.add(arm.rightArm);
-    camera.setTarget(scene.position);
+    cameraControl.setTarget(new THREE.Vector3(95, 240, 110));
+    camera = cameraControl.camera1;
 
     animate();
 }
@@ -28,6 +28,32 @@ function main() {
 main();
 
 window.addEventListener("resize", () => {
-    camera.update();
+    cameraControl.update();
     renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case '1':
+            console.log("[INFO]: camera1");
+            camera = cameraControl.camera1;
+            break;
+        case '2':
+            console.log("[INFO]: camera2");
+            camera = cameraControl.camera2;
+            break;
+        case '3':
+            console.log("[INFO]: camera3");
+            camera = cameraControl.camera3;
+            break;
+        case '4':
+            console.log("[INFO]: camera4");
+            camera = cameraControl.camera4;
+            break;
+        case '5':
+            console.log("[INFO]: camera5");
+            camera = cameraControl.camera5;
+            break;
+        // TODO: add the rest of the keys
+    }
 });
