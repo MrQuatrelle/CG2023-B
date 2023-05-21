d00d6fb45689cf51f39b50b6d2d5956447843db3
António Barros e Cunha <antoniobcunha@tecnico.ulisboa.pt>
2023-05-21 17:35:27 +0100

    small fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import cameraControl from "./camera.js";
-
+import body from "./body.js";
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 
@@ -21,6 +21,7 @@ function main() {
     scene.add(new THREE.AxesHelper(400));
     cameraControl.setTarget(new THREE.Vector3(95, 240, 110));
     camera = cameraControl.camera1;
+    scene.add(body.body);
 
     animate();
 }
