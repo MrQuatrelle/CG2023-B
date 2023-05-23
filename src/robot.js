cefe01f50365dc5d6596c78fc7a550a08cee14fe
MrQuatrelle <maigasmendo@gmail.com>
2023-05-23 11:53:12 +0100

    formatting robot.js

--- src/robot.js
+++ src/robot.js
@@ -5,21 +5,22 @@ import legs from "./legs.js"
 import head from "./head.js"
 
 
-class Robot extends THREE.Object3D{
-	#body
-	#head
+class Robot extends THREE.Object3D {
+    #body
+    #head
     #leftLeg;
     #rightLeg;
     #leftFoot;
     #rightFoot;
     #leftArm;
     #rightArm;
-    constructor(){
-    	super();
+
+    constructor() {
+        super();
         this.#head = new head.Head();
         this.#head.position.set(0, 60, 40);
 
-    	this.#body = new body.Body();
+        this.#body = new body.Body();
 
         this.#leftLeg = new legs.Leg();
         this.#leftFoot = this.#leftLeg.getFoot();
@@ -34,17 +35,20 @@ class Robot extends THREE.Object3D{
         this.#rightLeg.isRight();
 
         this.#rightArm = new arm.Arm();
-        this.#rightArm.position.set(-95, -25,-35);
+        this.#rightArm.position.set(-95, -25, -35);
         this.#leftArm = new arm.Arm();
-        this.#leftArm.position.set(95,-25,-35);
+        this.#leftArm.position.set(95, -25, -35);
         this.reset();
-        this.add(this.#body, this.#head, this.#head, this.#leftLeg, this.#rightLeg,this.#leftArm, this.#rightArm);
+        this.add(this.#body, this.#head,
+            this.#leftLeg, this.#rightLeg,
+            this.#leftArm, this.#rightArm);
     }
+
     reset() {
         this.position.set(110, 380, 80);
     }
 }
 
 export default {
-	Robot: Robot
+    Robot: Robot
 }
