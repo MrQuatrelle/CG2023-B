6b3d27942327b0453416f83f0f0d21ebdcc2421e
MrQuatrelle <maigasmendo@gmail.com>
2023-05-23 16:27:22 +0100

    added movement to trailer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import cameraControl from "./camera.js";
 import robot from "./robot.js";
+import tow from "./tow.js";
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 
-var beepboop;
+var beepboop, trailer;
 var camera;
 
 function animate() {
@@ -26,7 +27,10 @@ function main() {
     camera = cameraControl.camera1;
 
     beepboop = new robot.Robot();
-    scene.add(beepboop);
+    trailer = new tow.Tow();
+    trailer.position.set(400, 140, 240);
+
+    scene.add(beepboop, trailer);
 
     animate();
 }
@@ -41,27 +45,27 @@ window.addEventListener("resize", () => {
 window.addEventListener("keydown", (e) => {
     switch (e.key) {
         case '1':
-            console.log("[INFO]: camera1");
+            console.log("[INFO]: showing camera1");
             camera = cameraControl.camera1;
             break;
 
         case '2':
-            console.log("[INFO]: camera2");
+            console.log("[INFO]: showing camera2");
             camera = cameraControl.camera2;
             break;
 
         case '3':
-            console.log("[INFO]: camera3");
+            console.log("[INFO]: showing camera3");
             camera = cameraControl.camera3;
             break;
 
         case '4':
-            console.log("[INFO]: camera4");
+            console.log("[INFO]: showing camera4");
             camera = cameraControl.camera4;
             break;
 
         case '5':
-            console.log("[INFO]: camera5");
+            console.log("[INFO]: showing camera5");
             camera = cameraControl.camera5;
             break;
 
@@ -83,6 +87,31 @@ window.addEventListener("keydown", (e) => {
         case 'a':
             console.log("[INFO]: moving feet down");
             beepboop.moveFeetDown();
+            break;
+
+       case 'ArrowDown':
+           console.log("[INFO]: moving tow +x");
+           trailer.translateX(5);
+           break;
+            
+       case 'ArrowUp':
+           console.log("[INFO]: moving tow -x");
+           trailer.translateX(-5);
+           break;
+            
+       case 'ArrowLeft':
+           console.log("[INFO]: moving tow +z");
+           trailer.translateZ(5);
+           break;
+
+       case 'ArrowRight':
+           console.log("[INFO]: moving tow -z");
+           trailer.translateZ(-5);
+           break;
+
+        // for debugging purposes
+        default:
+            console.log(e);
             break;
 
         // TODO: add the rest of the keybinds here
